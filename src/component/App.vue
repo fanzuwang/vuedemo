c1379a4474b745bf3313214608d56f45ec3dd5a6
fzw <1870092453@qq.com>
2018-02-27 16:50:35 +0800

    再次修改

---
+++ src/component/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        {{val}}
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                val:'vue脚手架测试1111111'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
